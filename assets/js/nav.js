3b59731b2df40548d62abe787785c0c4c267e5d4
VulaegisSafety <vulaegis.safety+github@gmail.com>
2025-07-02 04:05:56 +0300

    Create nav.js

---
+++ assets/js/nav.js
@@ -0,0 +1,15 @@
+function openNav() {
+  document.getElementById("slideNav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("slideNav").style.width = "0";
+}
+
+// Close when clicking outside
+document.addEventListener('click', function(event) {
+  const slideNav = document.getElementById('slideNav');
+  if (!event.target.closest('.slide-nav') && !event.target.closest('.menu-btn')) {
+    slideNav.style.width = "0";
+  }
+});
